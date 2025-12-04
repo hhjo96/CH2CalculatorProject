bd0be4ce8ec7dc78bcc0e1dd092ca8b56dbbfc06
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-04 15:21:34 +0900

    반복문을 사용하되, 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기 구현

--- src/Step1/Calculator.java
+++ src/Step1/Calculator.java
@@ -1,35 +1,53 @@
 package Step1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        int number1 = 0;
+        int number2 = 0;
+        int result = 0;
+        String message = ""; // 결과 식을 String으로 저장
+        String exitMessage = ""; // exit인지를 입력받을 변수
 
-        //숫자 입력받기
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int number1 = sc.nextInt();
+        while(true) {
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int number2 = sc.nextInt();
+            //숫자 입력받기
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            number1 = sc.nextInt();
 
-        //엔터 지우기
-        sc.nextLine();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String operator = sc.nextLine();
-        operator = operator.charAt(0)+ "";
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            number2 = sc.nextInt();
+
+            //엔터 지우기
+            sc.nextLine();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String operator = sc.nextLine();
+            operator = operator.charAt(0) + "";
 
 //        System.out.println("operator = " + operator);
 
-        //계산하기. 추후 결과를 저장해야 하므로 message 에 저장했다.
-        int result = calc(number1, number2, operator);
+            //계산하기. 추후 결과를 저장해야 하므로 message 에 저장했다.
+            result = calc(number1, number2, operator);
 
-        //나누기 0의 경우 따로 처리
-        if(operator.equals("/") && number2 == 0){
-            System.out.println("프로그램을 종료합니다. ");
-        } else {
-            String message = number1 + operator + number2 + " = " + result;
-            System.out.println(message);
+            //나누기 0의 경우 따로 처리
+            if (operator.equals("/") && number2 == 0) {
+                continue;
+            } else {
+                message = number1 + operator + number2 + " = " + result;
+                System.out.println(message);
+            }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            exitMessage = sc.nextLine();
+            if (exitMessage.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
         }
 
     }
