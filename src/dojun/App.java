ff376afc1bab096c1da98c59ccd3f9861d06b0df
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-05 10:29:51 +0900

    출력문들을 메인함수 밖의 메서드로 뺐다

--- src/dojun/App.java
+++ src/dojun/App.java
@@ -1,8 +1,5 @@
 package dojun;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -18,7 +15,6 @@ public class App {
 
         String operator = "";
         String exitMessage = ""; // exit인지를 입력받을 변수
-        String deleteMessage = ""; // 가장 먼저 저장된 데이터 삭제할지 입력받을 변수
 
         OperatorType op = null; // enum으로 바꾸기
 
@@ -31,9 +27,7 @@ public class App {
             try {
                 number1 = sc.nextInt();
             } catch (Exception e) {
-                System.out.println("숫자만 입력해주세요.");
-                //엔터 지우기
-                sc.nextLine();
+                printOnlyNumbersAndDeleteEnters(sc);
                 continue;
             }
 
@@ -41,9 +35,7 @@ public class App {
             try {
                 number2 = sc.nextInt();
             } catch (Exception e) {
-                System.out.println("숫자만 입력해주세요.");
-                //엔터 지우기
-                sc.nextLine();
+                printOnlyNumbersAndDeleteEnters(sc);
                 continue;
             }
 
@@ -55,18 +47,9 @@ public class App {
             operator = operator.charAt(0) + "";
 
             if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
-                System.out.print("정의된 연산자는 ");
-
-                for (OperatorType opType : OperatorType.values()) {
-                    System.out.print(opType.getOperator() + " ");
-                }
-
-                System.out.println("입니다. 해당 연산자 중에서 입력해주세요.");
+                printOperatorType();
                 continue;
             }
-
-//        System.out.println("operator = " + operator);
-
 
             //계산하기.
             result = calculator.calc(number1, number2, operator);
@@ -78,15 +61,7 @@ public class App {
                 System.out.println("현재까지의 결과 값: " + calculator.getResultList());
             }
 
-            System.out.println("가장 먼저 저장된 값을 삭제하시겠습니까? (y or n)");
-            deleteMessage = sc.nextLine();
-            if (deleteMessage.equalsIgnoreCase("y")) {
-                calculator.removeResultList();
-
-                System.out.println("가장 먼저 저장된 값을 삭제했습니다.");
-                System.out.println("현재까지의 결과 값: " + calculator.getResultList());
-            }
-
+            printYNRemoveList(sc, calculator);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             exitMessage = sc.nextLine();
@@ -98,6 +73,38 @@ public class App {
 
         }
 
+    }
+
+    //메인 함수를 깔끔하게 만들기 위해 별도의 함수로 만들어 보았다.
+    public static void printOnlyNumbersAndDeleteEnters(Scanner sc) {
+
+        System.out.println("숫자만 입력해주세요.");
+        //엔터 지우기
+        sc.nextLine();
+
+    }
+
+    public static void printOperatorType() {
+
+        System.out.print("정의된 연산자는 ");
+
+        for (OperatorType opType : OperatorType.values()) {
+            System.out.print(opType.getOperator() + " ");
+        }
+
+        System.out.println("입니다. 해당 연산자 중에서 입력해주세요.");
+    }
+
+    public static void printYNRemoveList(Scanner sc, Calculator calculator){
+
+        System.out.println("가장 먼저 저장된 값을 삭제하시겠습니까? (y or n)");
+        String deleteMessage = sc.nextLine(); // 가장 먼저 저장된 데이터 삭제할지 입력받을 변수
+        if (deleteMessage.equalsIgnoreCase("y")) {
+            calculator.removeResultList();
+        }
+
+        System.out.println("가장 먼저 저장된 값을 삭제했습니다.");
+        System.out.println("현재까지의 결과 값: " + calculator.getResultList());
     }
 
 }
