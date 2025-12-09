718766601499e9d93f4061f644045436010f4d9f
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-09 19:34:52 +0900

    추가: 람다/스트림 조회 조건 추가

--- src/step3/App.java
+++ src/step3/App.java
@@ -3,6 +3,7 @@ package step3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 //
@@ -49,12 +50,13 @@ public class App {
             operator = sc.nextLine();
             operator = operator.charAt(0) + "";
 
+            //정해진 연산자가 아니라면 다시입력하라고 하기
             if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
                 printOperatorType();
                 continue;
             }
 
-            //계산하기.
+            //계산하기
             result = calculator.calc(calculator.number1, calculator.number2, operator);
 
             //나누기 0의 경우 따로 처리
@@ -64,6 +66,7 @@ public class App {
                 System.out.println("현재까지의 결과 값: " + calculator.getResultList());
             }
 
+            //가장먼저 저장된값 삭제
             System.out.println("가장 먼저 저장된 값을 삭제하시겠습니까? (y or n)");
             String deleteMessage = sc.nextLine(); // 가장 먼저 저장된 데이터 삭제할지 입력받을 변수
             if (deleteMessage.equalsIgnoreCase("y")) {
@@ -71,7 +74,11 @@ public class App {
                 printRemoveList(calculator);
             }
 
+            //입력받은 값보다 큰 결과 출력하기
             printBiggerNum(calculator, sc);
+
+            //추가: 연산자별 결과 출력하기
+            printByOp(calculator, sc);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             exitMessage = sc.nextLine();
@@ -94,6 +101,7 @@ public class App {
 
     }
 
+    //정의된 연산자만 출력하라고 하는 메서드
     public static void printOperatorType() {
 
         System.out.print("정의된 연산자는 ");
@@ -105,12 +113,14 @@ public class App {
         System.out.println("입니다. 해당 연산자 중에서 입력해주세요.");
     }
 
+    //가장먼저 저장된 값 삭제
     public static void printRemoveList(Calculator<Double> calculator){
 
         System.out.println("가장 먼저 저장된 값을 삭제했습니다.");
         System.out.println("현재까지의 결과 값: " + calculator.getResultList());
     }
 
+    //입력받은 값보다 큰 결과만 출력
     public static void printBiggerNum(Calculator<Double> calculator, Scanner sc) {
         System.out.println("입력받은 값보다 큰 결과물을 출력하시겠습니까? (y or n)");
         String bigMessage = sc.nextLine();
@@ -129,6 +139,43 @@ public class App {
                 }
             }
             System.out.println();
+        }
+    }
+
+    //연산자를 입력받아 그 연산자로 계산한 계산식을 출력
+    public static void printByOp(Calculator<Double> calculator, Scanner sc) {
+        System.out.println("연산자 별로 출력하시겠습니까? (y or n)");
+        String temp = sc.nextLine();
+        if (temp.equalsIgnoreCase("y")) {
+            while(true) {
+                System.out.println("출력할 연산자의 종류를 입력해주세요.");
+                String operator = sc.nextLine();
+                switch (operator) {
+                    case "+":
+                        System.out.println("---덧셈---");
+                        System.out.println(calculator.getResultList().stream().filter(r -> r.contains(operator)).collect(Collectors.toList()));
+                        break;
+                    case "-":
+                        System.out.println("---뺄셈---");
+                        System.out.println(calculator.getResultList().stream().filter(r -> r.contains(operator)).collect(Collectors.toList()));
+                        break;
+                    case "*":
+                        System.out.println("---곱셈---");
+                        System.out.println(calculator.getResultList().stream().filter(r -> r.contains(operator)).collect(Collectors.toList()));
+                        break;
+                    case "/":
+                        System.out.println("---나눗셈---");
+                        System.out.println(calculator.getResultList().stream().filter(r -> r.contains(operator)).collect(Collectors.toList()));
+                        break;
+                    default:
+                        System.out.println("잘못된 연산자를 입력했습니다. + - * / 중에 입력해주세요.");
+                }
+                System.out.println("연산자별 출력을 계속하시겠습니까? (exit 입력 시 종료)");
+                String exitMessage = sc.nextLine();
+                if (exitMessage.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
         }
     }
 }
